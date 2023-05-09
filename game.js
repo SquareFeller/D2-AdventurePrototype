9feef4aa24431b4fc47e37815484613940617737
Isai Rincon <Rinconisai@gmail.com>
2023-05-08 19:36:06 -0700

    Added 3 audio assets and tweaked more code.

--- game.js
+++ game.js
@@ -138,14 +138,14 @@ class Stairs_Right extends AdventureScene {
     preload() {
         this.load.image('floor', 'assets/no_key_on_floor.png');
         this.load.image('first key', 'assets/first_key.png');
+        this.load.audio('collect', 'assets/collect.wav');
     }
     onEnter() {
         this.makebg('floor');
-
+        let c = this.sound.add('collect');
         let b;
         let check = 0;
         let k = this.add.sprite(660, 680, "first key").setInteractive().setScale(1.2, 1.2);
-        //k.angle = -15;
         let glow = k.preFX.addGlow();
         if (this.hasItem('key')) {
             glow.setActive(false);
@@ -154,7 +154,8 @@ class Stairs_Right extends AdventureScene {
         k.on('pointerdown', () => {
             this.gainItem('key');
             if (check == 0) {
-                this.showMessage("Obtained key!")
+                this.showMessage("Obtained key!");
+                c.play({ volume: 0.25 });
             } else {
                 this.showMessage("There's nothing else to take.");
             }
@@ -245,13 +246,21 @@ class Transition extends Phaser.Scene {
     constructor() {
         super('transition');
     }
+    preload() {
+        this.load.audio('footsteps', 'assets/footsteps.wav');
+    }
     create() {
+        let f = this.sound.add('footsteps');
+        f.play();
         let message = this.add.text(25, 25,
             `With the door opened, I stepped into a labyrinth. Bare walls and pipes surrounded me and the relics of old machines greeted me. I made my way through winding paths and confusing routes until I was greeted by another interesting door...`).setFontSize(45);
         message.setWordWrapWidth(1500);
         this.time.delayedCall(6500, () => {
             this.cameras.main.fade(1000, 0, 0, 0);
-            this.time.delayedCall(1000, () => this.scene.start('final_door'));
+            this.time.delayedCall(1000, () => {
+                this.scene.start('final_door');
+                f.stop();
+            });
         })
     }
 }
@@ -342,10 +351,12 @@ class Locker_Interior extends AdventureScene {
     preload() {
         this.load.image('locker_interior', 'assets/key_in_locker.jpg');
         this.load.image('end_key', 'assets/ending_key.png');
+        this.load.audio('collect', 'assets/collect.wav')
     }
     onEnter() {
         this.makebg('locker_interior');
         let b;
+        let c = this.sound.add('collect');
         let final_key = this.add.sprite(540, 950, "end_key").setScale(0.85, 0.85).setInteractive();
         let g = final_key.preFX.addGlow();
         let check = 0;
@@ -358,7 +369,8 @@ class Locker_Interior extends AdventureScene {
         final_key.on('pointerdown', () => {
             this.gainItem('key');
             if (check == 0) {
-                this.showMessage("Obtained key!")
+                this.showMessage("Obtained key!");
+                c.play({ volume: 0.25 });
             } else {
                 this.showMessage("There's nothing else to take.");
             }
@@ -409,17 +421,6 @@ class Click extends Phaser.Scene {
         super('click');
     }
     create() {
-        //debug tools
-        this.input.keyboard.on('keyup-' + 'O', () => {
-            this.scene.start('basement_exterior');
-        });
-        this.input.keyboard.on('keyup-' + 'K', () => this.scene.start('right'));
-        this.input.keyboard.on('keyup-' + 'D', () => this.scene.start('first_door'));
-        this.input.keyboard.on('keyup-' + 'F', () => this.scene.start('final_door'));
-        this.input.keyboard.on('keyup-' + 'L', () => this.scene.start('lockers'));
-
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
         this.add.text(655, 540, "Click to begin.").setFontSize(55);
         this.input.on('pointerdown', () => {
             this.cameras.main.fade(1000, 0, 0, 0);
@@ -432,7 +433,12 @@ class Title extends Phaser.Scene {
     constructor() {
         super('title');
     }
+    preload() {
+        this.load.audio('title', 'assets/title_screen.wav');
+    }
     create() {
+        let t = this.sound.add('title')
+        t.play({ loop: true });
         this.add.text(655, 355, "89").setFontSize(55);
         let play = this.add.text(655, 540, "Begin").setFontSize(45);
         play.setInteractive();
@@ -446,7 +452,10 @@ class Title extends Phaser.Scene {
         });
         play.on('pointerdown', () => {
             this.cameras.main.fade(1000, 0, 0, 0);
-            this.time.delayedCall(1000, () => this.scene.start('basement_exterior'));
+            this.time.delayedCall(1000, () => {
+                this.scene.start('basement_exterior');
+                t.stop()
+            });
         });
     }
 }
@@ -458,7 +467,7 @@ class Outro extends Phaser.Scene {
     }
     create() {
         this.add.text(50, 50, "You wish you hadn't opened the door. You weren't sure why you were here in the first place, but you never imagined it would lead to this. Before more regret seeped in, you were dragged in by that THING. Never to be seen again.").setFontSize(50).setWordWrapWidth(1500);
-        this.add.text(50, 300, "Click anywhere to restart.").setFontSize(20);
+        this.time.delayedCall(5000, () => this.add.text(50, 300, "Click anywhere to restart.").setFontSize(20));
         this.input.on('pointerdown', () => this.scene.start('studio'));
     }
 }
